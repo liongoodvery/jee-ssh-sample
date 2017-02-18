69dc9c28df0eca95439c41e975fe4533998869fe
lion_good <lion.good.very.fisrst@gmail.com>
2017-02-18 22:44:43 +0800

    init

---
+++ src/main/java/org/lion/HelloWorl.java
@@ -0,0 +1,7 @@
+package org.lion;
+
+/**
+ * Created by more on 2017/02/18 0018.
+ */
+public class HelloWorl {
+}
---
+++ src/test/java/org/lion/SampleTest.java
@@ -0,0 +1,13 @@
+package org.lion;
+
+import org.junit.Test;
+
+/**
+ * Created by more on 2017/02/18 0018.
+ */
+public class SampleTest {
+    @Test
+    public void test8() throws Exception {
+
+    }
+}
