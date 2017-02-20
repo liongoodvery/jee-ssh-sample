260dda5037c2888c10725afd6f3892658341679a
lion <lion_good@163.com>
2017-02-20 15:03:21 +0800

    struct2 demo

---
+++ src/main/java/org/lion/action/HelloAction.java
@@ -0,0 +1,11 @@
+package org.lion.action;
+
+/**
+ * Created by lion on 2/20/17.
+ */
+public class HelloAction {
+    public String sayHello() {
+        System.out.println("hello");
+        return null;
+    }
+}
